bb7fd84d960584caa8f184576b7fe6ec488e1ebb
Sebastian Vivas <sebasvivas107@gmail.com>
2024-02-07 11:29:39 -0400

    Vim configuration

---
+++ after/plugin/defaults.lua
@@ -0,0 +1,23 @@
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Basics
+vim.keymap.set("n", "<leader>w",  ":w<CR>")
+vim.keymap.set("n", "<leader>q",  ":q<CR>")
+vim.keymap.set("n", "<leader>Q",  ":q!<CR>")
+vim.keymap.set("i", ";;",  "<Esc>")
+
+-- Window Navigation
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+
+-- Tabs navigation
+vim.keymap.set("n", "<leader>k", ":tabn<CR>")
+vim.keymap.set("n", "<leader>j", ":tabp<CR>")
+
+-- Split screen
+vim.keymap.set("n", "<leader>vs", ":vsplit<CR>")
+
+-- New Tab
+vim.keymap.set("n", "<leader>t", ":tabnew<CR>")
--- init.lua
+++ init.lua
@@ -271,7 +271,7 @@ require('lazy').setup({
   --    Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --
   --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
 }, {})
 
 -- [[ Setting options ]]
@@ -329,7 +329,7 @@ vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = tr
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+-- vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
